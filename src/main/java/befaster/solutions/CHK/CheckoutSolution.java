cb8749f2349818033d49150f0978d4256e09cd6e
sbx_user1051 <sbx_user1051@169.254.18.213>
2024-03-22 20:09:47 +0000

    Fri Mar 22 20:09:47 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -43,7 +43,7 @@ public class CheckoutSolution {
         // Apply special offer for item 'E': buy 2 E's, get one B free
         int eCount = itemCounts.getOrDefault('E', 0);
         int freeBs = eCount / 2;
-        total += (eCount - freeBs) * prices.get('E');
+        total += (eCount * prices.get('E')) - (freeBs * prices.get('B'));
 
         // Calculate total for remaining items
         for (Map.Entry<Character, Integer> entry : itemCounts.entrySet()) {
@@ -57,6 +57,7 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
 
