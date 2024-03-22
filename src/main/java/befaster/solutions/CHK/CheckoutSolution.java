8ab8c4858a3195aaafd27a276dc953c2dae17029
sbx_user1051 <sbx_user1051@169.254.18.213>
2024-03-22 20:19:36 +0000

    Fri Mar 22 20:19:36 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -14,7 +14,7 @@ public class CheckoutSolution {
         prices.put('B', 30);
         prices.put('C', 20);
         prices.put('D', 15);
-        prices.put('E', 20); // Corrected price for item E
+        prices.put('E', 40); // Price of item E
 
         // Initialize total checkout value
         int total = 0;
@@ -58,5 +58,6 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
