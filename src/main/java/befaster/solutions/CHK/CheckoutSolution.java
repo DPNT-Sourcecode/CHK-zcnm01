6c45b7b602f01a5479ff39c34a0a9b26e737a9c5
sbx_user1051 <sbx_user1051@169.254.12.189>
2024-03-23 13:24:55 +0000

    Sat Mar 23 13:24:55 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -10,6 +10,7 @@ public class CheckoutSolution {
 
 
     public Integer checkout(String skus) {
+
         // Price table and special offers
         Map<Character, Integer> prices = new HashMap<>();
         prices.put('A', 50);
@@ -30,31 +31,33 @@ public class CheckoutSolution {
             itemCounts.put(sku, itemCounts.getOrDefault(sku, 0) + 1);
         }
 
+
         // Apply special offer for SKU 'A' and calculate total checkout value
         int aCount = itemCounts.getOrDefault('A', 0);
         int offerPriceA3 = 130;
         int offerPriceA5 = 200;
         total += (aCount / 5) * offerPriceA5 + ((aCount % 5) / 3) * offerPriceA3 + ((aCount % 5) % 3) * prices.get('A');
 
-        // Apply special offer for SKU 'B'
+
         int bCount = itemCounts.getOrDefault('B', 0);
-        int offerQuantityB = 2;
-        int offerPriceB = 45;
-        total += (bCount / offerQuantityB) * offerPriceB + (bCount % offerQuantityB) * prices.get('B');
 
         // Apply special offer for item 'E': buy 2 E's, get one B free
         int eCount = itemCounts.getOrDefault('E', 0);
 
-        total += prices.get('E')*eCount;
-        if(bCount>0)
-        {
-            if(eCount>=2 && bCount==1) total-=prices.get('B');
-            else if(eCount>=4 && bCount==2) total-=offerPriceB;
-            else if(eCount>=6 && bCount==3) total-=prices.get('B')+offerPriceB;
-            else if(eCount>=8 && bCount==4) total-=prices.get('B')*2*offerPriceB;
+        total += prices.get('E') * eCount;
+        if (bCount > 0) {
+            if (eCount >= 2 && bCount >= 1) bCount--;
+            else if (eCount >= 4 && bCount >= 2) bCount -= 2;
+            else if (eCount >= 6 && bCount >= 3) bCount -= 3;
+            else if (eCount >= 8 && bCount >= 4) bCount -= 4;
         }
 
-        if(aCount==2 && bCount == 2 && eCount == 2) total=280;
+        // Apply special offer for SKU 'B'
+
+        int offerQuantityB = 2;
+        int offerPriceB = 45;
+        total += (bCount / offerQuantityB) * offerPriceB + (bCount % offerQuantityB) * prices.get('B');
+
 
         // Calculate total for remaining items
         for (Map.Entry<Character, Integer> entry : itemCounts.entrySet()) {
@@ -66,5 +69,6 @@ public class CheckoutSolution {
         }
 
         return total;
+
     }
 }
